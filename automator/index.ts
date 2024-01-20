e912913ca82b084c97840bf660314e10621afadd
Ivan Chub <ivansergchub@gmail.com>
2024-01-20 14:51:17 -0800

    remember session between selenium restarts

--- automator/index.ts
+++ automator/index.ts
@@ -1,14 +1,44 @@
-import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
-import { sleep } from './util';
+import {
+  Builder,
+  Browser,
+  By,
+  Key,
+  until,
+  WebDriver,
+  ChromiumWebDriver,
+  Capabilities,
+} from "selenium-webdriver";
+import { sleep } from "./util";
+import { Options } from "selenium-webdriver/chrome.js";
+import * as fs from "fs";
+import * as path from "path";
+
+const BROWSER_DATA_DIR_NAME = "browser_data";
+const BROWSER_DATA_PATH = path.resolve(path.join("./", BROWSER_DATA_DIR_NAME));
 
 (async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  var chromeCapabilities = Capabilities.chrome();
+  //setting chrome options to start the browser fully maximized
+  var chromeOptions = {
+    args: ["--test-type", "--start-maximized"],
+  };
+  chromeCapabilities.set("chromeOptions", chromeOptions);
+
+  const options = new Options();
+  options.windowSize({ width: 100, height: 100 });
+  options.addArguments(`user-data-dir=${BROWSER_DATA_PATH}`);
+
+  let driver = await new Builder()
+    .withCapabilities(chromeCapabilities)
+    .setChromeOptions(options)
+    .forBrowser(Browser.CHROME)
+    .build();
 
   try {
-    await driver.get('https://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    await sleep(5000);
+    await driver.get("https://tinder.com/app/recs");
+    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await sleep(500000);
   } finally {
     await driver.quit();
   }
