670583630dd8230e2464b2dfb26828f90ae7bd4f
aki <akshath@live.com>
2024-01-20 14:48:40 -0800

    init express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('gm');
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+// Handle 404s
+app.use((req, res) => {
+  console.error(`404: ${req.method} ${req.url}`);
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+// Handle server errors
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    console.error('\x1b[31m', err.stack, '\x1b[0m');
+    res.status(err.status || 500).json({ message: err.message, error: err });
+  },
+);
+
+app.listen(PORT, () => {
+  console.info(`Listening on port ${PORT}...`);
+});
