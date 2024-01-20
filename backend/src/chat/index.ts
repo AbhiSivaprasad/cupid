a0bb77b75153b8dcf2b099c4aac7caeb024a1989
aki <akshath@live.com>
2024-01-20 15:26:31 -0800

    dummy data to test chat

--- backend/src/chat/index.ts
+++ backend/src/chat/index.ts
@@ -6,10 +6,13 @@ export function getMessage(
   selfProfile: Profile,
   otherProfile: Profile,
   messages: Message[],
+  debug = false,
 ) {
+  const prompt = constructPrompt(selfProfile, otherProfile, messages);
+  if (debug) {
+    console.log('Prompt:', prompt);
+  }
+
   const gpt = new GPT('gpt-4-1106-preview');
-  return gpt.complete(
-    constructPrompt(selfProfile, otherProfile, messages),
-    300,
-  );
+  return gpt.complete(prompt, 300);
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,24 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import { getMessage } from './chat';
+import { Profile } from './chat/types';
 
-const app = express();
-const PORT = 3000;
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
-});
+const DUMMY_ME: Profile = {
+  texts: [
+    "I'm a software engineer at OpenAI.",
+    'I like to play the piano and go hiking.',
+  ],
+  images: ['https://example.com/me.jpg'],
+};
+const DUMMY_THEM: Profile = {
+  texts: [
+    "I'm a software engineer at Google.",
+    'I like to play the guitar and travel the world.',
+  ],
+  images: ['https://example.com/them.jpg'],
+};
 
-// Handle 404s
-app.use((req, res) => {
-  console.error(`404: ${req.method} ${req.url}`);
-  return res.status(404).json({ message: 'Not Found' });
-});
-
-// Handle server errors
-app.use(
-  (
-    err: any,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
-    console.error('\x1b[31m', err.stack, '\x1b[0m');
-    res.status(err.status || 500).json({ message: err.message, error: err });
-  },
-);
-
-app.listen(PORT, () => {
-  console.info(`Listening on port ${PORT}...`);
-});
+(async () => {
+  console.log('Message:', await getMessage(DUMMY_ME, DUMMY_THEM, [], true));
+})();
