2adbf46deaf8d39590227bb68e87573342a5f43e
Ivan Chub <ivansergchub@gmail.com>
2024-01-20 14:22:50 -0800

    setting up selenium

--- automator/index.ts
+++ automator/index.ts
@@ -1,1 +1,15 @@
-console.log("test")
+import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
+import { sleep } from './util';
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+  try {
+    await driver.get('https://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await sleep(5000);
+  } finally {
+    await driver.quit();
+  }
+})();
