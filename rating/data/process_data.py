df97b410867bbec8156f5539223ab48f4a8b595a
Abhi Sivaprasad <abhi@live.com>
2024-01-20 17:37:47 -0800

    updated data processing

---
+++ rating/data/process_data.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+from pathlib import Path
+from glob import glob
+import sys
+sys.path.append("/Users/abhisivaprasad/Documents/projects/cupid/rating/")
+from embedding.deepface import get_image_annotations
+
+
+BASE_DATASET_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dataset")
+
+
+# generate embedding matricies
+def construct_training_data(base_dataset_path: str = BASE_DATASET_PATH):
+    base_dataset_path = Path(base_dataset_path)
+    negative_examples_paths = glob(str(base_dataset_path / "raw" / "no" / "*.jpeg"))
+    positive_examples_paths = glob(str(base_dataset_path / "raw" / "yes" / "*.jpeg"))
+    
+    # embed all images
+    negative_image_annotations = get_image_annotations(negative_examples_paths)
+    positive_image_annotations = get_image_annotations(positive_examples_paths)
+    negative_embeddings = negative_image_annotations.embeddings
+    positive_embeddings = positive_image_annotations.embeddings
+
+    # combine and make reponder
+    embeddings = np.vstack([negative_embeddings, positive_embeddings])
+    zeros = np.zeros(negative_embeddings.shape[0])
+    ones = np.ones(positive_embeddings.shape[0])
+    responder = np.concatenate([zeros, ones])
+
+    # write data
+    np.save(base_dataset_path / "processed" / "X.npy", embeddings)
+    np.save(base_dataset_path / "processed" / "y.npy", responder)
+
+
+if __name__ == "__main__":
+    construct_training_data(BASE_DATASET_PATH)
