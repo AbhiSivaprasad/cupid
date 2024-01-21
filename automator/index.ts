ec5fee33bf09208d8a2d3adf3dc5e5a62415cbd7
Ivan Chub <ivansergchub@gmail.com>
2024-01-20 17:05:12 -0800

    implemented liking profiles as well

--- automator/index.ts
+++ automator/index.ts
@@ -177,7 +177,22 @@ export async function clickOnElement(
 }
 
 export async function getLikeButton(driver: WebDriver): Promise<WebElement> {
-  return null;
+  const buttons = await driver.findElements(By.css("button"));
+
+  for (const button of buttons) {
+    try {
+      const path = await button.findElement(By.css("span span svg path"));
+      const d = await path.getAttribute("d");
+      if (
+        d ===
+        "M21.994 10.225c0-3.598-2.395-6.212-5.72-6.212-1.78 0-2.737.647-4.27 2.135C10.463 4.66 9.505 4 7.732 4 4.407 4 2 6.62 2 10.231c0 1.52.537 2.95 1.533 4.076l8.024 7.357c.246.22.647.22.886 0l7.247-6.58.44-.401.162-.182.168-.174a6.152 6.152 0 0 0 1.54-4.09"
+      ) {
+        return button;
+      }
+    } catch (e) {}
+  }
+
+  throw new Error("unable to find like button");
 }
 
 export async function getDislikeButton(driver: WebDriver): Promise<WebElement> {
@@ -246,8 +261,9 @@ export async function extractCurrentProfile(
   }
 
   try {
+    const likeButton = await getLikeButton(driver);
     const dislikeButton = await getDislikeButton(driver);
-    await clickOnElement(driver, dislikeButton);
+    await clickOnElement(driver, likeButton);
   } catch (e) {
     // console.log("failed to click on the dislike button", e);
   }
