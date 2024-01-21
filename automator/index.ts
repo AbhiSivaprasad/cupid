6a049aa01e578f90b4624dbc684ab66d72981228
aki <akshath@live.com>
2024-01-20 17:23:14 -0800

    refactor driver creation

--- automator/index.ts
+++ automator/index.ts
@@ -1,43 +1,21 @@
-import {
-  Builder,
-  Browser,
-  By,
-  Key,
-  until,
-  WebDriver,
-  ChromiumWebDriver,
-  Capabilities,
-  WebElement,
-  Actions,
-  Origin,
-} from "selenium-webdriver";
-import { sleep } from "./util";
-import { Options } from "selenium-webdriver/chrome.js";
 import * as fs from "fs";
 import * as path from "path";
-import { isNamespaceExport } from "typescript";
+import {
+  Browser,
+  Builder,
+  By,
+  Capabilities,
+  WebDriver,
+  WebElement,
+} from "selenium-webdriver";
+import { Options } from "selenium-webdriver/chrome.js";
+import { sleep } from "./util";
 
 const BROWSER_DATA_DIR_NAME = "browser_data";
 const BROWSER_DATA_PATH = path.resolve(path.join("./", BROWSER_DATA_DIR_NAME));
 
 (async function example() {
-  var chromeCapabilities = Capabilities.chrome();
-  //setting chrome options to start the browser fully maximized
-  var chromeOptions = {
-    args: ["--test-type", "--start-maximized"],
-  };
-  chromeCapabilities.set("chromeOptions", chromeOptions);
-
-  const options = new Options();
-  // options.windowSize({ width: 100, height: 100 });
-  options.addArguments(`user-data-dir=${BROWSER_DATA_PATH}`);
-
-  let driver = await new Builder()
-    .withCapabilities(chromeCapabilities)
-    .setChromeOptions(options)
-    .forBrowser(Browser.CHROME)
-    .build();
-
+  const driver = await getDriver();
   try {
     await driver.get("https://tinder.com/app/recs");
     await sleep(5_000);
@@ -80,6 +58,25 @@ const BROWSER_DATA_PATH = path.resolve(path.join("./", BROWSER_DATA_DIR_NAME));
 //
 //  input: settings object
 //
+
+export function getDriver(): Promise<WebDriver> {
+  var chromeCapabilities = Capabilities.chrome();
+  //setting chrome options to start the browser fully maximized
+  var chromeOptions = {
+    args: ["--test-type", "--start-maximized"],
+  };
+  chromeCapabilities.set("chromeOptions", chromeOptions);
+
+  const options = new Options();
+  // options.windowSize({ width: 100, height: 100 });
+  options.addArguments(`user-data-dir=${BROWSER_DATA_PATH}`);
+
+  return new Builder()
+    .withCapabilities(chromeCapabilities)
+    .setChromeOptions(options)
+    .forBrowser(Browser.CHROME)
+    .build();
+}
 
 /**
  * Profile of someone we're currently looking at and need to decide
